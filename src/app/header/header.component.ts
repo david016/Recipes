d8badd924b546bf3d7154b67b01920dbc8bb9aa3
David Varga <davidvarga986@gmail.com>
2023-07-03 13:29:26 +0200

    Enable navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  // showingRecepies = true; // else ingridients
+  @Output()
+  navigationChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  changeNavigation(recepies: boolean): void {
+    // this.showingRecepies = !this.showingRecepies;
+    // console.log(this.showingRecepies);
+
+    this.navigationChanged.emit(recepies);
+  }
 }
