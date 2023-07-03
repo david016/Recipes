24d14395bad19d3609cd07d9be8098d5c4ff5f97
David Varga <davidvarga986@gmail.com>
2023-07-03 16:53:50 +0200

    Add recipe detail on click

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,20 +7,32 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  selectedRecipe: Recipe = null;
+
   recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
+      'A test recipe1',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'https://images.unsplash.com/photo-1602271886918-bafecc837c7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
     ),
     new Recipe(
-      'A test recipe',
+      'A test recipe2',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'https://images.unsplash.com/photo-1602271886918-bafecc837c7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
     ),
   ];
 
   constructor() {}
+
+  // showRecipe() {
+  //   console.log('helo there');
+  // }
+  showDetails(recipe: Recipe) {
+    console.log('recipe: ', recipe);
+    this.selectedRecipe = recipe;
+    console.log('selected recipe:', this.selectedRecipe);
+  }
+
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
   }
