b9320a0d279818371a083f2cab558bd16537bd32
DavidVarga <davidvarga986@gmail.com>
2023-07-17 00:50:10 +0200

    add recipes to service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,20 +10,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   selectedRecipe: Recipe = null;
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe1',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      'https://images.unsplash.com/photo-1602271886918-bafecc837c7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-    ),
-    new Recipe(
-      'A test recipe2',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      'https://www.maggi.co.uk/sites/default/files/styles/maggi_desktop_image_style/public/NUK1265%20maggi%20Recipes%20banner%201500x700px%20opt2A.jpg?h=4f5b30f1&itok=DcsF1RwA'
-    ),
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() {}
+  constructor(public recipeService: RecipeService) {}
 
   // showRecipe() {
   //   console.log('helo there');
@@ -34,6 +24,6 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // throw new Error('Method not implemented.');
+    this.recipes = this.recipeService.recipes;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe1',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'https://images.unsplash.com/photo-1602271886918-bafecc837c7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
+    ),
+    new Recipe(
+      'A test recipe2',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'https://www.maggi.co.uk/sites/default/files/styles/maggi_desktop_image_style/public/NUK1265%20maggi%20Recipes%20banner%201500x700px%20opt2A.jpg?h=4f5b30f1&itok=DcsF1RwA'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 }
